0d8a796fbdafeaaa68d91c3d3f8dd672522834a1
Florian Mold <florian.mold@live.at>
2019-08-09 17:29:39 +0200

    Revert "[TASK] remove unnecessary files"
    This reverts commit 6abe170245eb15d1d116b80a6e093ad774f0d847.

---
+++ test/User.test.ts
@@ -0,0 +1,15 @@
+// /*import DummyClass from "../src/serious-game-library"
+//
+// /**
+//  * Dummy test
+//  */
+// describe("Dummy test", () => {
+//   it("works if true is truthy", () => {
+//     expect(true).toBeTruthy()
+//   })
+//
+//   it("DummyClass is instantiable", () => {
+//     expect(new DummyClass()).toBeInstanceOf(DummyClass)
+//   })
+// })*/
+
