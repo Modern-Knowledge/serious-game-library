a746c71b5102fe81e01a7468cd2cc03cd27e8aef
Florian Mold <florian.mold@live.at>
2019-08-15 18:36:50 +0200

    [TASK] fix

--- src/utils/Stopwatch.ts
+++ src/utils/Stopwatch.ts
@@ -1,16 +1,14 @@
-export class Stopwatch {
+/* export class Stopwatch {
   private readonly _name: string;
-  private _start: [number, number] = process.hrtime();
+  private _start: [number, number] = console.time();
 
   public constructor(name: string) {
     this._name = name;
   }
 
-  /**
-   *
-   */
+
   public getTimeElapsed(): string {
     const end = process.hrtime(this._start);
     return `${this._name}: ${Math.round((end[0] * 1000) + (end[1] / 1000000))}s`;
   }
-}
+}*/
