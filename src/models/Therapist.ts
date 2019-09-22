6151aacb537e6dd6fc0dc251d4f2f9bf96406408
Daniel Kaufmann <e1639360@student.tuwien.ac.at>
2019-09-22 14:51:24 +0200

    [FEATURE] deserialize model data to receive model instead of plain json

--- src/models/Therapist.ts
+++ src/models/Therapist.ts
@@ -1,28 +1,35 @@
-import { User } from "./User";
-import { Patient } from "./Patient";
+import { User } from './User'
+import { Patient } from './Patient'
+import { Deserializable } from './Deserializable'
 
-export class Therapist extends User {
+export class Therapist extends User implements Deserializable {
+  private _patients: Patient[] = []
 
-    private _patients: Patient[] = [];
+  public constructor() {
+    super()
+  }
 
-    public constructor() {
-        super();
-    }
+  public deserialize(input: any) {
+    Object.assign(this, input)
+    this.patients = this.patients.map(patient => {
+      return new Patient().deserialize(patient)
+    })
+    return this
+  }
 
-    get patients(): Patient[] {
-        return this._patients;
-    }
+  get patients(): Patient[] {
+    return this._patients
+  }
 
-    set patients(value: Patient[]) {
-        this._patients = value;
-    }
+  set patients(value: Patient[]) {
+    this._patients = value
+  }
 
-    public addPatient(patient: Patient): void {
-        this._patients.push(patient);
-    }
+  public addPatient(patient: Patient): void {
+    this._patients.push(patient)
+  }
 
-    public addPatients(patients: Patient[]): void {
-        this._patients = this._patients.concat(patients);
-    }
-
+  public addPatients(patients: Patient[]): void {
+    this._patients = this._patients.concat(patients)
+  }
 }
--- src/models/User.ts
+++ src/models/User.ts
@@ -1,101 +1,107 @@
-import { AbstractModel } from "./AbstractModel";
-import { Status } from "../enums/Status";
-import { Session } from "./Session";
+import { AbstractModel } from './AbstractModel'
+import { Status } from '../enums/Status'
+import { Session } from './Session'
+import { Deserializable } from './Deserializable'
 
-export class User extends AbstractModel {
-  private _email: string;
-  private _password: string;
-  private _forename: string;
-  private _lastname: string;
-  private _lastLogin: Date;
-  private _failedLoginAttempts: number;
-  private _loginCoolDown: Date;
-  private _status: Status;
+export class User extends AbstractModel implements Deserializable {
+  private _email: string
+  private _password: string
+  private _forename: string
+  private _lastname: string
+  private _lastLogin: Date
+  private _failedLoginAttempts: number
+  private _loginCoolDown: Date
+  private _status: Status
 
-  private _sessions: Session[] = [];
+  private _sessions: Session[] = []
 
   public constructor() {
-    super();
+    super()
+  }
+
+  public deserialize(input: any) {
+    Object.assign(this, input)
+    return this
   }
 
   get email(): string {
-    return this._email;
+    return this._email
   }
 
   set email(value: string) {
-    this._email = value;
+    this._email = value
   }
 
   get password(): string {
-    return this._password;
+    return this._password
   }
 
   set password(value: string) {
-    this._password = value;
+    this._password = value
   }
 
   get forename(): string {
-    return this._forename;
+    return this._forename
   }
 
   set forename(value: string) {
-    this._forename = value;
+    this._forename = value
   }
 
   get lastname(): string {
-    return this._lastname;
+    return this._lastname
   }
 
   set lastname(value: string) {
-    this._lastname = value;
+    this._lastname = value
   }
 
   get lastLogin(): Date {
-    return this._lastLogin;
+    return this._lastLogin
   }
 
   set lastLogin(value: Date) {
-    this._lastLogin = value;
+    this._lastLogin = value
   }
 
   get failedLoginAttempts(): number {
-    return this._failedLoginAttempts;
+    return this._failedLoginAttempts
   }
 
   set failedLoginAttempts(value: number) {
-    this._failedLoginAttempts = value;
+    this._failedLoginAttempts = value
   }
 
   get loginCoolDown(): Date {
-    return this._loginCoolDown;
+    return this._loginCoolDown
   }
 
   set loginCoolDown(value: Date) {
-    this._loginCoolDown = value;
+    this._loginCoolDown = value
   }
 
   get status(): Status {
-    return this._status;
+    return this._status
   }
 
   set status(value: Status) {
-    this._status = value;
+    this._status = value
   }
 
   get sessions(): Session[] {
-    return this._sessions;
+    return this._sessions
   }
 
   set sessions(value: Session[]) {
-    this._sessions = value;
+    this._sessions = value
   }
 
   public addSession(session: Session): void {
-    this._sessions.push(session);
+    this._sessions.push(session)
   }
 
   public addSessions(sessions: Session[]): void {
-    this._sessions = this._sessions.concat(sessions);
+    this._sessions = this._sessions.concat(sessions)
   }
 
   get fullName() {
