b9a6ab671595487624dd76cd4d4da3ce0c2116d4
Florian Mold <florian.mold@live.at>
2019-09-28 21:46:51 +0200

    [FEATURE] add date-time formatter

---
+++ src/utils/dateFormatter.ts
@@ -0,0 +1,28 @@
+import moment from "moment";
+
+/**
+ * format the passed Date as date time
+ * e.g. 01.01.1970 12:00
+ * @param date
+ */
+export function formatDateTime(date: Date): string {
+  return moment(date).format("L LT");
+}
+
+/**
+ * format the passed Date as date
+ * e.g. 01.01.1970
+ * @param date
+ */
+export function formatDate(date: Date): string {
+  return moment(date).format("L");
+}
+
+/**
+ * format the passed Date as time
+ * e.g. 12:00
+ * @param date
+ */
+export function formatTime(date: Date): string {
+  return moment(date).format("LT");
+}
