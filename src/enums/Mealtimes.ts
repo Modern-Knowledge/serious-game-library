1d64c33c3cd36e9b494e196c63791f81a4ec1275
Florian Mold <florian.mold@live.at>
2019-10-04 20:34:07 +0200

    [FEATURE] add mealtimes enum

---
+++ src/enums/Mealtimes.ts
@@ -0,0 +1,10 @@
+/*
+ * Copyright (c) 2019 Florian Mold
+ * All rights reserved.
+ */
+
+export const enum Mealtimes {
+    BREAKFAST = "Frühstück",
+    LUNCH = "Mittagessen",
+    DINNER = "Abendessen"
+}
