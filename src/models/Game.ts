9d19118ee7fa5a3a83feb583955584c952812393
Daniel Kaufmann <e1639360@student.tuwien.ac.at>
2019-11-11 00:39:07 +0100

    [BUGFIX] deserialize game error texts

--- src/models/Game.ts
+++ src/models/Game.ts
@@ -1,100 +1,102 @@
-import { AbstractModel } from "./AbstractModel";
-import { Helptext } from "./Helptext";
-import { GameSetting } from "./GameSetting";
-import { Errortext } from "./Errortext";
+import { AbstractModel } from './AbstractModel';
+import { Errortext } from './Errortext';
+import { GameSetting } from './GameSetting';
+import { Helptext } from './Helptext';
 
 export class Game extends AbstractModel<Game> {
-  private _name: string;
-  private _description: string;
+  private _name: string
+  private _description: string
 
-  private _helptexts: Helptext[] = [];
-  private _gameSettings: GameSetting[] = [];
-  private _errortexts: Errortext[] = [];
+  private _helptexts: Helptext[] = []
+  private _gameSettings: GameSetting[] = []
+  private _errortexts: Errortext[] = []
 
-  private _component: string;
+  private _component: string
 
   /**
    * helptext array converted to helptext map
    */
-  private _helptextMap: Map<string, Helptext> = undefined;
+  private _helptextMap: Map<string, Helptext> = undefined
 
   public constructor() {
-    super();
+    super()
   }
 
   public deserialize(input: any) {
-    Object.assign(this, input);
+    Object.assign(this, input)
     this.helptexts = this.helptexts.map(helptext => {
-      return new Helptext().deserialize(helptext);
-    });
+      return new Helptext().deserialize(helptext)
+    })
+    this.errortexts = this.errortexts.map(errortext => {
+      return new Errortext().deserialize(errortext)
+    })
 
     this.gameSettings = this.gameSettings.map(gameSetting => {
-      return new GameSetting().deserialize(gameSetting);
-    });
-    return this;
+      return new GameSetting().deserialize(gameSetting)
+    })
+    return this
   }
 
   get name(): string {
-    return this._name;
+    return this._name
   }
 
   set name(value: string) {
-    this._name = value;
+    this._name = value
   }
 
   get description(): string {
-    return this._description;
+    return this._description
   }
 
   set description(value: string) {
-    this._description = value;
+    this._description = value
   }
 
   get helptexts(): Helptext[] {
-    return this._helptexts;
+    return this._helptexts
   }
 
   set helptexts(value: Helptext[]) {
-    this._helptexts = value;
+    this._helptexts = value
   }
 
   get errortexts(): Errortext[] {
-    return this._errortexts;
+    return this._errortexts
   }
 
   set errortexts(value: Errortext[]) {
-    this._errortexts = value;
+    this._errortexts = value
   }
 
   get gameSettings(): GameSetting[] {
-    return this._gameSettings;
+    return this._gameSettings
   }
 
   set gameSettings(value: GameSetting[]) {
-    this._gameSettings = value;
+    this._gameSettings = value
   }
 
   get component(): string {
-    return this._component;
+    return this._component
   }
 
   set component(value: string) {
-    this._component = value;
+    this._component = value
   }
 
   /**
    * reads helptexts[] into map
    */
   public createHelptextMap(): void {
-    this._helptextMap = new Map<string, Helptext>();
+    this._helptextMap = new Map<string, Helptext>()
 
     for (const helpText of this._helptexts) {
-      this._helptextMap.set(helpText.name, helpText);
+      this._helptextMap.set(helpText.name, helpText)
     }
   }
 
   get helptextMap(): Map<string, Helptext> {
-    return this._helptextMap;
+    return this._helptextMap
   }
-
 }
