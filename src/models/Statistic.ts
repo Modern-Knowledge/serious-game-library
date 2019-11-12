807c38c6ed824b78f677110065890d705c8a3984
Daniel Kaufmann <e1639360@student.tuwien.ac.at>
2019-11-12 22:17:01 +0100

    [BUGFIX] serialize error texts of statistic

--- src/models/Statistic.ts
+++ src/models/Statistic.ts
@@ -1,49 +1,52 @@
-import { Deserializable } from "../interfaces/Deserializable";
-import { AbstractModel } from "./AbstractModel";
-import { Errortext } from "./Errortext";
+import { Deserializable } from '../interfaces/Deserializable';
+import { AbstractModel } from './AbstractModel';
+import { Errortext } from './Errortext';
 
-export class Statistic extends AbstractModel<Statistic> implements Deserializable {
-    private _startTime: Date;
-    private _endTime: Date;
+export class Statistic extends AbstractModel<Statistic>
+  implements Deserializable {
+  private _startTime: Date;
+  private _endTime: Date;
 
-    private _errortexts: Errortext[] = [];
+  private _errortexts: Errortext[] = [];
 
-    public constructor() {
-        super();
+  public constructor() {
+    super();
+  }
+
+  public deserialize(input: any) {
+    if (input && input._startTime) {
+      this._startTime = new Date(input._startTime);
     }
-
-    public deserialize(input: any) {
-        if (input && input._startTime) {
-            this._startTime = new Date(input._startTime);
-        }
-        if (input && input._endTime) {
-            this._endTime = new Date(input._endTime);
-        }
-        return this;
+    if (input && input._endTime) {
+      this._endTime = new Date(input._endTime);
     }
+    this.errortexts = this.errortexts.map((errortext: any) => {
+      return new Errortext().deserialize(errortext);
+    });
+    return this;
+  }
 
-    get startTime(): Date {
-        return this._startTime;
-    }
+  get startTime(): Date {
+    return this._startTime;
+  }
 
-    set startTime(value: Date) {
-        this._startTime = value;
-    }
+  set startTime(value: Date) {
+    this._startTime = value;
+  }
 
-    get endTime(): Date {
-        return this._endTime;
-    }
+  get endTime(): Date {
+    return this._endTime;
+  }
 
-    set endTime(value: Date) {
-        this._endTime = value;
-    }
+  set endTime(value: Date) {
+    this._endTime = value;
+  }
 
-    get errortexts(): Errortext[] {
-        return this._errortexts;
-    }
+  get errortexts(): Errortext[] {
+    return this._errortexts;
+  }
 
-    set errortexts(value: Errortext[]) {
-        this._errortexts = value;
-    }
-
+  set errortexts(value: Errortext[]) {
+    this._errortexts = value;
+  }
 }
