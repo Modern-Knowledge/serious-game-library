4a39a2ffca2f2d265e4d5c10576ab4c177f5865b
Daniel Kaufmann <e1639360@student.tuwien.ac.at>
2019-11-18 21:49:41 +0100

    [FEATURE] adjust templateParser for frontend usage

--- src/utils/TemplateParser.ts
+++ src/utils/TemplateParser.ts
@@ -2,13 +2,15 @@
  * replace placeholder variables in texts
  */
 export class TemplateParser {
-    private readonly _params: string[] = [];
+    private _params: string[] = [];
 
     /**
      * @param params params that should be replaced in the text
      */
-    public constructor(params: string[]) {
-        this._params = params;
+    public constructor(params?: string[]) {
+        if (params) {
+            this._params = params;
+        }
     }
 
     /**
@@ -16,24 +18,29 @@ export class TemplateParser {
      * if variable amount does not match the specified, warnings and errors are printed
      * @param text string that ist processed
      */
-    public parse(text: string): string {
+    public parse(text: string, params?: string[]): string {
         const regexp: RegExp = new RegExp("::(.*?)::", "g");
         const matched: RegExpMatchArray = text.match(regexp);
+        const parameters = params ? params : this._params;
 
         if (matched === null) {
             const errorStr = `No variables to replace!`;
             throw new Error(errorStr);
         }
 
-        if (matched.length > this._params.length) {
+        if (matched.length > parameters.length) {
             const errorStr = `Not enough replacement variables passed!`;
             throw new Error(errorStr);
         }
 
         for (let i = 0; i < matched.length; i++) {
-            text = text.replace(matched[i], this._params[i]);
+            text = text.replace(matched[i], parameters[i]);
         }
 
         return text;
+    }
+
+    set params(value: string[]) {
+        this._params = value;
     }
 }
