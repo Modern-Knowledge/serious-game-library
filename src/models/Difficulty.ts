9d3187c1ab5ad16d9600c3145de15b3ca21c8101
Florian Mold <florian.mold@live.at>
2020-01-23 16:56:26 +0100

    [TASK] recipe-info page

--- src/models/Difficulty.ts
+++ src/models/Difficulty.ts
@@ -1,23 +1,40 @@
+import {Difficulties} from "../enums/Difficulties";
 import { IDeserializable } from "../interfaces/IDeserializable";
 import { AbstractModel } from "./AbstractModel";
 
 export class Difficulty extends AbstractModel<Difficulty> implements IDeserializable {
-  private _difficulty: number;
+    private _difficulty: number;
 
-  public constructor() {
-    super();
-  }
+    public constructor() {
+        super();
+    }
 
-  public deserialize(input: any) {
-    Object.assign(this, input);
-    return this;
-  }
+    public deserialize(input: any) {
+        Object.assign(this, input);
+        return this;
+    }
 
-  get difficulty(): number {
-    return this._difficulty;
-  }
+    get difficulty(): number {
+        return this._difficulty;
+    }
 
-  set difficulty(value: number) {
-    this._difficulty = value;
-  }
+    set difficulty(value: number) {
+        this._difficulty = value;
+    }
+
+    /**
+     * Returns the string representation of the difficulty. If the difficulty is not known an empty string is
+     * returned.
+     */
+    get difficultyName(): string {
+        if (this._difficulty === Difficulties.EASY) {
+            return "Leicht";
+        } else if (this._difficulty === Difficulties.MEDIUM) {
+            return "Mittel";
+        } else if (this._difficulty === Difficulties.HARD) {
+            return "Schwer";
+        }
+
+        return "";
+    }
 }
--- src/models/Recipe.ts
+++ src/models/Recipe.ts
@@ -6,72 +6,76 @@ import { Difficulty } from "./Difficulty";
 import { Ingredient } from "./Ingredient";
 
 export class Recipe extends AbstractModel<Recipe> implements IDeserializable {
-  private _name: string;
-  private _description: string;
-  private _difficultyId: number;
-  private _mealtime: Mealtimes;
+    private _name: string;
+    private _description: string;
+    private _difficultyId: number;
+    private _mealtime: Mealtimes;
 
-  private _difficulty: Difficulty = undefined;
-  private _ingredients: Ingredient[] = [];
+    private _difficulty: Difficulty = undefined;
+    private _ingredients: Ingredient[] = [];
 
-  public constructor() {
-    super();
-  }
+    public constructor() {
+        super();
+    }
 
-  public deserialize(input: any) {
-    Object.assign(this, input);
-    this.difficulty = new Difficulty().deserialize(input.difficulty);
-    this.ingredients = this.ingredients.map((ingredient) => {
-      return new Ingredient().deserialize(ingredient);
-    });
-    return this;
-  }
+    public deserialize(input: any) {
+        Object.assign(this, input);
+        this.difficulty = new Difficulty().deserialize(input._difficulty);
+        this.ingredients = this.ingredients.map((ingredient) => {
+            return new Ingredient().deserialize(ingredient);
+        });
+        return this;
+    }
 
-  get name(): string {
-    return this._name;
-  }
+    get name(): string {
+        return this._name;
+    }
 
-  set name(value: string) {
-    this._name = value;
-  }
+    set name(value: string) {
+        this._name = value;
+    }
 
-  get description(): string {
-    return this._description;
-  }
+    get description(): string {
+        return this._description;
+    }
 
-  set description(value: string) {
-    this._description = value;
-  }
+    set description(value: string) {
+        this._description = value;
+    }
 
-  get mealtime(): Mealtimes {
-    return this._mealtime;
-  }
+    get mealtime(): Mealtimes {
+        return this._mealtime;
+    }
 
-  set mealtime(value: Mealtimes) {
-    this._mealtime = value;
-  }
+    set mealtime(value: Mealtimes) {
+        this._mealtime = value;
+    }
 
-  get difficultyId(): number {
-    return this._difficultyId;
-  }
+    get difficultyId(): number {
+        return this._difficultyId;
+    }
 
-  set difficultyId(value: number) {
-    this._difficultyId = value;
-  }
+    set difficultyId(value: number) {
+        this._difficultyId = value;
+    }
 
-  get difficulty(): Difficulty {
-    return this._difficulty;
-  }
+    get difficulty(): Difficulty {
+        return this._difficulty;
+    }
 
-  set difficulty(value: Difficulty) {
-    this._difficulty = value;
-  }
+    set difficulty(value: Difficulty) {
+        this._difficulty = value;
+    }
 
-  get ingredients(): Ingredient[] {
-    return this._ingredients;
-  }
+    get ingredients(): Ingredient[] {
+        return this._ingredients;
+    }
 
-  set ingredients(value: Ingredient[]) {
-    this._ingredients = value;
-  }
+    set ingredients(value: Ingredient[]) {
+        this._ingredients = value;
+    }
+
+    get commaSeparatedIngredientNames(): string {
+        return this._ingredients.map((i) => i.name).join(", ");
+    }
 }
