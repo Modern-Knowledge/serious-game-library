1912621243e69a9f38e604c772267d0a8551ee8e
Daniel Kaufmann <e1639360@student.tuwien.ac.at>
2020-02-21 16:32:55 +0100

    [BUGFIX] fix therapist patient deserializing

--- src/models/Dto/TherapistDto.ts
+++ src/models/Dto/TherapistDto.ts
@@ -1,21 +1,25 @@
-import {IDeserializable} from "../../interfaces/IDeserializable";
-import {Patient} from "../Patient";
-import {Therapist} from "../Therapist";
-import {PatientDto} from "./PatientDto";
-import {UserDto} from "./UserDto";
+import { IDeserializable } from "../../interfaces/IDeserializable";
+import { Patient } from "../Patient";
+import { Therapist } from "../Therapist";
+import { PatientDto } from "./PatientDto";
+import { UserDto } from "./UserDto";
 
 export class TherapistDto extends UserDto implements IDeserializable {
     private _patients: PatientDto[] = [];
 
     public constructor(therapist?: Therapist) {
         super(therapist);
-        this._patients = therapist.patients.map((value: Patient) => new PatientDto(value));
+        this._patients = therapist.patients.map(
+            (value: Patient) => new PatientDto(value)
+        );
     }
 
     public deserialize(input: any) {
         Object.assign(this, input);
         this._patients = this._patients.map((patient: PatientDto) => {
-            return new PatientDto().deserialize(patient);
+            return new PatientDto(
+                new Patient().deserialize(patient)
+            ).deserialize(patient);
         });
         return this;
     }
