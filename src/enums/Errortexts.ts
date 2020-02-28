f91dbcff738b57a623249a0432cfce91271edb0a
Daniel Kaufmann <e1639360@student.tuwien.ac.at>
2020-02-28 16:42:09 +0100

    [FEATURE] add errortext enum

---
+++ src/enums/Errortexts.ts
@@ -0,0 +1,8 @@
+export const enum Errortexts {
+    DAY_PLANNING = "day-planning",
+    SHOPPING_LIST = "shopping-list",
+    FRIDGE_NOT_CHECKED = "fridge-not-checked",
+    SHOPPING_CART = "shopping-cart",
+    MEALTIME = "mealtime",
+    MEALTIME_FILLED = "mealtime-filled"
+}
