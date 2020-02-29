275d51bc4ced482650de3d4a7f9b50f8a4edf817
florian <florian.mold@live.at>
2020-02-29 11:15:31 +0100

    [BUGFIX] remove ionic-logger from library

--- src/utils/errorTextHelper.ts
+++ src/utils/errorTextHelper.ts
@@ -1,20 +1,17 @@
-import { Logger, LoggingService } from "ionic-logging-service";
-
 import { Errortext } from "../models/Errortext";
 
 /**
- * Gets an error text from by name.
- * @params error texts - The errortexts.
+ * Gets an error text from an array by name.
+ *
+ * @param errorTexts - Array of error-texts.
  * @param name - The name of the error text.
  * @returns - The specified error text.
  */
 export function getErrorText(errorTexts: Errortext[], name: string) {
-    const logger: Logger = new LoggingService().getLogger("getItemById");
     const foundErrorText = errorTexts.find(
         (errorText) => errorText.name === name
     );
     if (!foundErrorText) {
-        logger.error("Error text not found");
         return undefined;
     }
     return new Errortext().deserialize(foundErrorText);
