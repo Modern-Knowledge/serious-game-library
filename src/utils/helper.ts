efa4d6f0552a6be8f54528d10f57e2f7c107ff95
Daniel Kaufmann <e1639360@student.tuwien.ac.at>
2020-03-01 10:53:00 +0100

    [TASK] add shuffle helper function

--- src/utils/helper.ts
+++ src/utils/helper.ts
@@ -8,8 +8,22 @@ export function toMap(texts: Text[]): Map<string, Text> {
     const map = new Map<string, Text>();
 
     for (const item of texts) {
-       map.set(item.name, item);
+        map.set(item.name, item);
     }
 
     return map;
+}
+
+/**
+ * Shuffles items.
+ * Based on the modern Fisher-Yates-shuffle algorithm.
+ * [https://en.wikipedia.org/wiki/Fisher%E2%80%93Yates_shuffle#The_modern_algorithm]
+ * @param items The items to shuffle.
+ */
+export function shuffle(items: any[]) {
+    for (let i = items.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [items[i], items[j]] = [items[j], items[i]];
+    }
+    return items;
 }
